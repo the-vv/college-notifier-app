294900a261603912211b895f8cbef68d2f61df7c
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2022-02-09 14:12:40 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'college-notifier',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
